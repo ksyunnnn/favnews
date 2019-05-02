06534022222bca41241ae5ccc9b3fc03c25bc6f4
ksyunnnn <is.syunsukekobashi@gmail.com>
2019-05-02 14:31:31 +0900

    add API func, Context API

--- src/components/ArticleDetail/index.js
+++ src/components/ArticleDetail/index.js
@@ -6,7 +6,6 @@ export default ({ match }) => (
   <ArticlesContext.Consumer>
     {(articles) => {
       const article = articles.find(v => (v.title === match.params.title));
-      console.log(article);
       return (article && <span>{article.content}</span>);
     }}
   </ArticlesContext.Consumer>
